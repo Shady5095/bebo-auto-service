72965525255578cfd295252f2a26a5bc8222b6b6
shady5095 <shady.hesham10@gmail.com>
2023-11-26 09:54:49 +0200

    ca222

--- android/app/src/main/kotlin/com/example/bebo_auto_service_admin/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/bebo_auto_service_admin/MainActivity.kt
@@ -1,6 +1,15 @@
+<<<<<<< HEAD
 package com.example.bebo_auto_service_admin
 
 import io.flutter.embedding.android.FlutterActivity
 
 class MainActivity: FlutterActivity() {
 }
+=======
+package com.example.bebo_auto_service_admin
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
+>>>>>>> d6fc83e (car images)
